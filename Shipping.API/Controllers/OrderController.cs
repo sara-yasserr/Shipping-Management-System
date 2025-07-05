14638525e380612d3b2cc1b00a8cc949e5f78e9d
sara-yasserr <sarahyasser979@gmail.com>
2025-07-05 18:29:05 +0300

    assign agent(working) + Add pagination to Orders

--- Shipping.API/Controllers/OrderController.cs
+++ Shipping.API/Controllers/OrderController.cs
@@ -25,7 +25,7 @@ namespace Shipping.API.Controllers
             var orders = await _orderService.GetAllOrdersAsync(pagination);
             return Ok(orders);
         }
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}")]
         public async Task<ActionResult<ReadOrderDTO>> GetOrderById(int id)
         {
             var order = await _orderService.GetOrderById(id);
